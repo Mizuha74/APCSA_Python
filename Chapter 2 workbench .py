244da76df4b97655c2d9690cda18636a086115ca
Mizuha74 <eddyzhou0703@gmail.com>
2024-09-16 10:32:57 -0700

    Add files via upload

---
+++ Chapter 2 workbench .py
@@ -0,0 +1,23 @@
+# Chapter 2 workbench 1, 2, 4, 8, 9, 12
+#1
+def age(): 
+    age = int(input("Enter age: "))
+    print (age)
+
+#2
+def color():
+    color = input("Enter favorite color: ")
+    print (color)
+
+#4
+    #a result = 6
+    #b result = 4
+    #c result = 4
+    #d result = 6
+    #e result = 2.5
+    
+#8
+    #   11
+    
+#9
+    #   5
